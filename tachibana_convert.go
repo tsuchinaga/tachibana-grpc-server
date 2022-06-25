9b3c32735a3bdec0c5918693c989ad0e145e11af
tsuchinaga <woocun@gmail.com>
2022-06-26 05:51:01 +0900

    レスポンスの共通項目も返すよう変更

--- tachibana_convert.go
+++ tachibana_convert.go
@@ -15,6 +15,7 @@ func (t *tachibanaApi) toLoginRequest(req *pb.LoginRequest) *tachibana.LoginRequ
 
 func (t *tachibanaApi) fromLoginResponse(res *tachibana.LoginResponse) *pb.LoginResponse {
 	return &pb.LoginResponse{
+		CommonResponse:            t.fromCommonResponse(&res.CommonResponse),
 		ResultCode:                res.ResultCode,
 		ResultText:                res.ResultText,
 		AccountType:               t.fromAccountType(res.AccountType),
@@ -39,6 +40,127 @@ func (t *tachibanaApi) fromLoginResponse(res *tachibana.LoginResponse) *pb.Login
 		UnreadDocument:            res.UnreadDocument,
 		Token:                     "",
 	}
+}
+
+func (t *tachibanaApi) fromCommonResponse(res *tachibana.CommonResponse) *pb.CommonResponse {
+	return &pb.CommonResponse{
+		No:           res.No,
+		SendDate:     timestamppb.New(res.SendDate),
+		ReceiveDate:  timestamppb.New(res.ReceiveDate),
+		ErrorNo:      t.fromErrorNo(res.ErrorNo),
+		ErrorMessage: res.ErrorMessage,
+		MessageType:  t.fromMessageType(res.MessageType),
+	}
+}
+
+func (t *tachibanaApi) fromErrorNo(errorNo tachibana.ErrorNo) pb.ErrorNo {
+	switch errorNo {
+	case tachibana.ErrorNoProblem:
+		return pb.ErrorNo_ERROR_NO_NO_PROBLEM
+	case tachibana.ErrorNoData:
+		return pb.ErrorNo_ERROR_NO_NO_DATA
+	case tachibana.ErrorSessionInactive:
+		return pb.ErrorNo_ERROR_NO_SESSION_INACTIVE
+	case tachibana.ErrorProgressedNumber:
+		return pb.ErrorNo_ERROR_NO_PROGRESSED_NUMBER
+	case tachibana.ErrorExceedLimitTime:
+		return pb.ErrorNo_ERROR_NO_EXCEED_LIMIT_TIME
+	case tachibana.ErrorServiceOffline:
+		return pb.ErrorNo_ERROR_NO_SERVICE_OFFLINE
+	case tachibana.ErrorBadRequest:
+		return pb.ErrorNo_ERROR_NO_BAD_REQUEST
+	case tachibana.ErrorDatabaseAccess:
+		return pb.ErrorNo_ERROR_NO_DATABASE_ACCESS
+	case tachibana.ErrorServerAccess:
+		return pb.ErrorNo_ERROR_NO_SERVER_ACCESS
+	case tachibana.ErrorSystemOffline:
+		return pb.ErrorNo_ERROR_NO_SYSTEM_OFFLINE
+	case tachibana.ErrorOffHours:
+		return pb.ErrorNo_ERROR_NO_OFF_HOURS
+	}
+	return pb.ErrorNo_ERROR_NO_UNSPECIFIED
+}
+
+func (t *tachibanaApi) fromMessageType(messageType tachibana.MessageType) pb.MessageType {
+	switch messageType {
+	case tachibana.MessageTypeLoginRequest:
+		return pb.MessageType_MESSAGE_TYPE_LOGIN_REQUEST
+	case tachibana.MessageTypeLoginResponse:
+		return pb.MessageType_MESSAGE_TYPE_LOGIN_RESPONSE
+	case tachibana.MessageTypeLogoutRequest:
+		return pb.MessageType_MESSAGE_TYPE_LOGOUT_REQUEST
+	case tachibana.MessageTypeLogoutResponse:
+		return pb.MessageType_MESSAGE_TYPE_LOGOUT_RESPONSE
+	case tachibana.MessageTypeNewOrder:
+		return pb.MessageType_MESSAGE_TYPE_NEW_ORDER
+	case tachibana.MessageTypeCorrectOrder:
+		return pb.MessageType_MESSAGE_TYPE_CORRECT_ORDER
+	case tachibana.MessageTypeCancelOrder:
+		return pb.MessageType_MESSAGE_TYPE_CANCEL_ORDER
+	case tachibana.MessageTypeStockPositionList:
+		return pb.MessageType_MESSAGE_TYPE_STOCK_POSITION_LIST
+	case tachibana.MessageTypeMarginPositionList:
+		return pb.MessageType_MESSAGE_TYPE_MARGIN_POSITION_LIST
+	case tachibana.MessageTypeStockWallet:
+		return pb.MessageType_MESSAGE_TYPE_STOCK_WALLET
+	case tachibana.MessageTypeMarginWallet:
+		return pb.MessageType_MESSAGE_TYPE_MARGIN_WALLET
+	case tachibana.MessageTypeStockSellable:
+		return pb.MessageType_MESSAGE_TYPE_STOCK_SELLABLE
+	case tachibana.MessageTypeOrderList:
+		return pb.MessageType_MESSAGE_TYPE_ORDER_LIST
+	case tachibana.MessageTypeOrderDetail:
+		return pb.MessageType_MESSAGE_TYPE_ORDER_DETAIL
+	case tachibana.MessageTypeSummary:
+		return pb.MessageType_MESSAGE_TYPE_SUMMARY
+	case tachibana.MessageTypeSummaryRecord:
+		return pb.MessageType_MESSAGE_TYPE_SUMMARY_RECORD
+	case tachibana.MessageTypeStockEntryDetail:
+		return pb.MessageType_MESSAGE_TYPE_STOCK_ENTRY_DETAIL
+	case tachibana.MessageTypeMarginEntryDetail:
+		return pb.MessageType_MESSAGE_TYPE_MARGIN_ENTRY_DETAIL
+	case tachibana.MessageTypeDepositRate:
+		return pb.MessageType_MESSAGE_TYPE_DEPOSIT_RATE
+	case tachibana.MessageTypeEventDownload:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_DOWNLOAD
+	case tachibana.MessageTypeEventSystemStatus:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_SYSTEM_STATUS
+	case tachibana.MessageTypeBusinessDay:
+		return pb.MessageType_MESSAGE_TYPE_BUSINESS_DAY
+	case tachibana.MessageTypeTickGroup:
+		return pb.MessageType_MESSAGE_TYPE_TICK_GROUP
+	case tachibana.MessageTypeEventOperationStatus:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_OPERATION_STATUS
+	case tachibana.MessageTypeEventStockOperationStatus:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_STOCK_OPERATION_STATUS
+	case tachibana.MessageTypeEventProductOperationStatus:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_PRODUCT_OPERATION_STATUS
+	case tachibana.MessageTypeStockMaster:
+		return pb.MessageType_MESSAGE_TYPE_STOCK_MASTER
+	case tachibana.MessageTypeStockExchangeMaster:
+		return pb.MessageType_MESSAGE_TYPE_STOCK_EXCHANGE_MASTER
+	case tachibana.MessageTypeEventStockRegulation:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_STOCK_REGULATION
+	case tachibana.MessageTypeEventFutureMaster:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_FUTURE_MASTER
+	case tachibana.MessageTypeEventOptionMaster:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_OPTION_MASTER
+	case tachibana.MessageTypeEventExchangeRegulation:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_EXCHANGE_REGULATION
+	case tachibana.MessageTypeEventSubstitute:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_SUBSTITUTE
+	case tachibana.MessageTypeEventDepositMaster:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_DEPOSIT_MASTER
+	case tachibana.MessageTypeEventErrorReason:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_ERROR_REASON
+	case tachibana.MessageTypeEventDownloadComplete:
+		return pb.MessageType_MESSAGE_TYPE_EVENT_DOWNLOAD_COMPLETE
+	case tachibana.MessageTypeMasterData:
+		return pb.MessageType_MESSAGE_TYPE_MASTER_DATA
+	case tachibana.MessageTypeMarketPrice:
+		return pb.MessageType_MESSAGE_TYPE_MARKET_PRICE
+	}
+	return pb.MessageType_MESSAGE_TYPE_UNSPECIFIED
 }
 
 func (t *tachibanaApi) fromAccountType(accountType tachibana.AccountType) pb.AccountType {
